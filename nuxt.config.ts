3d5d80ee9ccb8fb3bcf70aeb840723c3ea14a724
Thai <thai.nguyen@webvi.vn>
2023-07-24 14:46:03 +0700

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,13 +13,17 @@ export default defineNuxtConfig({
       script: [
         {
           type: 'text/javascript',
-          src: '@/assets/js/app.js'
+          src: 'assets/js/app.js'
+        },
+        {
+          type: 'text/javascript',
+          src: '~/assets/js/app.js'
         }
       ],
       link: [
         {
           rel: 'icon',
-          href: '@/assets/images/favicon.ico'
+          href: '~/assets/images/favicon.ico'
         }
       ]
     }
